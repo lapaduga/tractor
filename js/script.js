1909f5fecd428e4f3c3466b920b056cb0cc9f6f0
lapaduga <deniskotov29042015@gmail.com>
2021-01-20 14:21:46 +0500

    menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	$('.header__burger').on('click', function () {
 		$('.header__burger, .menu-board').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
 });
