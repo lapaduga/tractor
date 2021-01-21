d9ae440ff8f5bd66b777ec07ea5c07c3f4d452ca
lapaduga <47709041+lapaduga@users.noreply.github.com>
2021-01-21 19:02:08 +0300

    so far

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
+
+	// burger menu
+
 	$('.header__burger').on('click', function () {
 		$('.header__burger, .menu-board').toggleClass('active');
 		$('body').toggleClass('lock');
+	});
+
+	// map
+
+	$('.footer__map-bar').on('click', function (){
+		$('.footer__see').fadeOut(300);
 	});
 });
