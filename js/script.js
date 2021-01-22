d76baa7c5d5f5afa24d7a22e3e89befb5f88b3fc
lapaduga <47709041+lapaduga@users.noreply.github.com>
2021-01-22 13:06:12 +0300

    header shrink and smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 $(document).ready(function () {
-
 	// burger menu
-
 	$('.header__burger').on('click', function () {
 		$('.header__burger, .menu-board').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 
-	// map
 
-	$('.footer__map-bar').on('click', function (){
+	//header height on scroll behaviour
+	window.onscroll = function () { scrollFunction() };
+	function scrollFunction() {
+		if (document.documentElement.clientWidth > 992) {
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				document.getElementById("header__body").style.height = "80px";
+				document.getElementById("header__logo").classList.add('shrink');
+			} else {
+				document.getElementById("header__body").style.height = "140px";
+				document.getElementById("header__logo").classList.remove('shrink');
+			}
+		}
+	}
+
+
+	// map
+	$('.footer__map-bar').on('click', function () {
 		$('.footer__see').fadeOut(300);
+	});
+
+
+	// smooth scroll
+	$("body").on('click', '[href*="#"]', function (e) {
+		var fixed_offset = 100;
+		$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+		e.preventDefault();
 	});
 });
