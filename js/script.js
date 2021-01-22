b4d38365e72afeb26a361b75be9e9f7b24e4d304
lapaduga <47709041+lapaduga@users.noreply.github.com>
2021-01-22 15:57:16 +0300

    sf

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ $(document).ready(function () {
 	//header height on scroll behaviour
 	window.onscroll = function () { scrollFunction() };
 	function scrollFunction() {
+		if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+			document.getElementById("header__body").style.background = "#070707";
+		} else {
+			document.getElementById("header__body").style.background = "transparent";
+		}
 		if (document.documentElement.clientWidth > 992) {
 			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 				document.getElementById("header__body").style.height = "80px";
@@ -17,6 +22,41 @@ $(document).ready(function () {
 				document.getElementById("header__body").style.height = "140px";
 				document.getElementById("header__logo").classList.remove('shrink');
 			}
+		}
+/* 		if (document.documentElement.clientWidth < 361) {
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				document.getElementById("header__logo").style.flex = "0 0 150px";
+			}
+		} else if (document.documentElement.clientWidth > 360 && document.documentElement.clientWidth < 993){
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				document.getElementById("header__logo").style.flex = "0 0 180px";
+			}
+		}
+	} */
+
+
+	// window resize 
+	window.onresize = function () { resizeFunction() };
+	function resizeFunction() {
+		if (document.documentElement.clientWidth > 992) {
+			if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+				document.getElementById("header__body").style.height = "80px";
+				document.getElementById("header__logo").classList.add('shrink');
+				document.getElementById("header__logo").style.flex = "0 0 180px";
+			}
+			else{
+				document.getElementById("header__logo").style.flex = "0 0 252px";
+				document.getElementById("header__body").style.height = "140px";
+				document.getElementById("header__logo").classList.remove('shrink');
+			}
+		}
+		if (document.documentElement.clientWidth > 360 && document.documentElement.clientWidth < 993) {
+			document.getElementById("header__body").style.height = "80px";
+		}
+		if (document.documentElement.clientWidth < 361) {
+			document.getElementById("header__logo").style.flex = "0 0 150px";
+		} else if (document.documentElement.clientWidth > 360 && document.documentElement.clientWidth < 993) {
+			document.getElementById("header__logo").style.flex = "0 0 180px";
 		}
 	}
 
