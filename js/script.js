dd89d68799755834d9aa38d6179df5970fe0a72c
lapaduga <47709041+lapaduga@users.noreply.github.com>
2021-02-01 09:31:06 +0300

    bvi

--- js/script.js
+++ js/script.js
@@ -181,48 +181,48 @@ $(document).ready(function () {
 	});
 
 	// contacts form validation
-/* 	form.addEventListener('submit', formSend);
-
-	async function formSend(e) {
-		e.preventDefault();
-
-		let error = formValidate(form);
-
-		let formData = new FormData(form);
-
-		if (error === 0) {
-			alert.classList.remove('active');
-			form.reset();
-		} else {
-			alert.classList.add('active');
-		}
-	}
+	/* 	form.addEventListener('submit', formSend);
 	
-	function formValidate(form) {
-		let error = 0;
-		let formReq = document.querySelectorAll('._req');
-
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);
-
-			if (input.value === '') {
-				formAddError(input);
-				error++;
+		async function formSend(e) {
+			e.preventDefault();
+	
+			let error = formValidate(form);
+	
+			let formData = new FormData(form);
+	
+			if (error === 0) {
+				alert.classList.remove('active');
+				form.reset();
+			} else {
+				alert.classList.add('active');
 			}
 		}
-		return error;
-	}
-
-	function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
-	}
-
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
-	} */
+		
+		function formValidate(form) {
+			let error = 0;
+			let formReq = document.querySelectorAll('._req');
+	
+			for (let index = 0; index < formReq.length; index++) {
+				const input = formReq[index];
+				formRemoveError(input);
+	
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+			return error;
+		}
+	
+		function formAddError(input) {
+			input.parentElement.classList.add('_error');
+			input.classList.add('_error');
+		}
+	
+		function formRemoveError(input) {
+			input.parentElement.classList.remove('_error');
+			input.classList.remove('_error');
+		} */
 
 	// teams tabs
 	$('.to-team').on('click', (e) => {
