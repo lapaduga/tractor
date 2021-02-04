4fcc8f641be7742dd132789056c15fbf6ff47729
lapaduga <47709041+lapaduga@users.noreply.github.com>
2021-02-04 11:24:24 +0300

    test

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-if ((window.location.pathname === '/') || (window.location.pathname === 'index.html/')) {
+/* if ((window.location.pathname === '/') || (window.location.pathname === 'index.html/')) {
 	if (localStorage.getItem('checked') === 'false') {
 		window.location.href = window.location.hostname + '/splash.html';
 	}
@@ -9,4 +9,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	} else {
 		document.getElementById('noshow').checked = false;
 	}
-})
+}) */
+
+if (localStorage.getItem('checked') === 'true') {
+	document.querySelector('.splash-main__checkbox').checked = true;
+	window.location.href = 'https://lapaduga.github.io/tractor/home.html';
+}
+if (localStorage.getItem('checked') === 'false') {
+	document.querySelector('.splash-main__checkbox').checked = false;
+}
+document.querySelector('.splash-main__label').onclick = () => {
+	if (document.querySelector('.splash-main__checkbox').checked === true) {
+		localStorage.setItem('checked', 'true');
+	}
+	if (document.querySelector('.splash-main__checkbox').checked === false) {
+		localStorage.setItem('checked', 'false');
+	}
+}
