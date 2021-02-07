9c8892f0447676715dfd5be3e0f5b3f3642f8ccc
lapaduga <deniskotov29042015@gmail.com>
2021-02-07 20:27:38 +0500

    video

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-/* if ((window.location.pathname === '/') || (window.location.pathname === 'index.html/')) {
-	if (localStorage.getItem('checked') === 'false') {
-		window.location.href = window.location.hostname + '/splash.html';
-	}
-}
-document.addEventListener("DOMContentLoaded", function (event) {
-	if (localStorage.getItem('checked') === 'true') {
-		document.getElementById('noshow').checked = true;
-	} else {
-		document.getElementById('noshow').checked = false;
-	}
-}) */
-
 if (localStorage.getItem('checked') === 'true') {
 	document.querySelector('.splash-main__checkbox').checked = true;
 	window.location.href = 'https://lapaduga.github.io/tractor/home.html';
